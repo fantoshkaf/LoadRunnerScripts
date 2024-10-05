2bd8638d3fd83b239bee71cb7f3237227c9a750d
fantoshkaf <fantoshkaf@mail.ru>
2024-10-05 19:21:08 +0300

    upd

--- DelBill/data/action.c
+++ DelBill/data/action.c
@@ -1,51 +1,333 @@
 Action()
 {
 
+	lr_start_transaction("UC5_DelSel");
+
+	lr_start_transaction("01_OpenWebTours");
+
 	web_set_sockets_option("SSL_VERSION", "2&3");
 
 	web_add_auto_header("Priority", 
-		"u=0, i");
+		"u=4");
 
 	web_add_auto_header("Upgrade-Insecure-Requests", 
 		"1");
 
-	web_url("WebTours", 
-		"URL=http://localhost:1080/WebTours/", 
+	web_custom_request("header.html", 
+		"URL=http://localhost:1080/WebTours/header.html", 
+		"Method=GET", 
 		"Resource=0", 
 		"RecContentType=text/html", 
-		"Referer=", 
+		"Referer=http://localhost:1080/WebTours/", 
 		"Snapshot=t1.inf", 
-		"Mode=HTML", 
+		"Mode=HTTP", 
 		LAST);
 
-	web_add_header("Origin", 
+	web_custom_request("welcome.pl", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=true", 
+		"Method=GET", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/WebTours/", 
+		"Snapshot=t3.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_revert_auto_header("Upgrade-Insecure-Requests");
+
+	web_add_auto_header("Priority", 
+		"u=5, i");
+
+	web_custom_request("hp_logo.png", 
+		"URL=http://localhost:1080/WebTours/images/hp_logo.png", 
+		"Method=GET", 
+		"Resource=1", 
+		"RecContentType=image/png", 
+		"Referer=http://localhost:1080/WebTours/header.html", 
+		"Snapshot=t4.inf", 
+		LAST);
+
+	web_custom_request("webtours.png", 
+		"URL=http://localhost:1080/WebTours/images/webtours.png", 
+		"Method=GET", 
+		"Resource=1", 
+		"RecContentType=image/png", 
+		"Referer=http://localhost:1080/WebTours/header.html", 
+		"Snapshot=t6.inf", 
+		LAST);
+
+	web_add_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_custom_request("nav.pl", 
+		"URL=http://localhost:1080/cgi-bin/nav.pl?in=home", 
+		"Method=GET", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?signOff=true", 
+		"Snapshot=t7.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	lr_end_transaction("01_OpenWebTours",LR_AUTO);
+
+	web_add_auto_header("Upgrade-Insecure-Requests", 
+		"1");
+
+	web_add_auto_header("Origin", 
 		"http://localhost:1080");
 
 	web_add_auto_header("Priority", 
 		"u=4");
 
-	lr_think_time(6);
+	lr_think_time(22);
 
-	web_submit_form("login.pl", 
-		"Snapshot=t3.inf", 
-		ITEMDATA, 
-		"Name=username", "Value=user1", ENDITEM, 
-		"Name=password", "Value=password1", ENDITEM, 
+	web_custom_request("login.pl", 
+		"URL=http://localhost:1080/cgi-bin/login.pl", 
+		"Method=POST", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home", 
+		"Snapshot=t9.inf", 
+		"Mode=HTTP", 
+		"Body=userSession=139980.150101256HcAAccipQHAiDDDDtDcAVpDHDicf&username=use1&password=pass1234&login.x=0&login.y=0&JSFormSubmit=off", 
 		LAST);
 
-	web_image("Itinerary Button", 
-		"Alt=Itinerary Button", 
-		"Snapshot=t4.inf", 
+	web_concurrent_start(NULL);
+
+	web_custom_request("error.pl", 
+		"URL=http://localhost:1080/cgi-bin/error.pl?error=badPassword", 
+		"Method=GET", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/login.pl", 
+		"Snapshot=t10.inf", 
+		"Mode=HTTP", 
 		LAST);
+
+	web_custom_request("nav.pl_2", 
+		"URL=http://localhost:1080/cgi-bin/nav.pl?username=use1&password=pass1234", 
+		"Method=GET", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/login.pl", 
+		"Snapshot=t11.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_concurrent_end(NULL);
+
+	lr_think_time(18);
+
+	lr_start_transaction("02_Login");
+
+	web_custom_request("login.pl_2", 
+		"URL=http://localhost:1080/cgi-bin/login.pl", 
+		"Method=POST", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?username=use1&password=pass1234", 
+		"Snapshot=t12.inf", 
+		"Mode=HTTP", 
+		"Body=userSession=139980.150101256HcAAccipQHAiDDDDtDcAVpDHDicf&username=user1&password=pass1234&login.x=61&login.y=7&JSFormSubmit=off", 
+		LAST);
+
+	web_concurrent_start(NULL);
+
+	web_custom_request("nav.pl_3", 
+		"URL=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Method=GET", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/login.pl", 
+		"Snapshot=t13.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_custom_request("login.pl_3", 
+		"URL=http://localhost:1080/cgi-bin/login.pl?intro=true", 
+		"Method=GET", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/login.pl", 
+		"Snapshot=t14.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_concurrent_end(NULL);
+
+	web_concurrent_start(NULL);
+
+	web_custom_request("flights.gif", 
+		"URL=http://localhost:1080/WebTours/images/flights.gif", 
+		"Method=GET", 
+		"Resource=1", 
+		"RecContentType=image/gif", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t15.inf", 
+		LAST);
+
+	web_custom_request("in_home.gif", 
+		"URL=http://localhost:1080/WebTours/images/in_home.gif", 
+		"Method=GET", 
+		"Resource=1", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t16.inf", 
+		LAST);
+
+	web_custom_request("itinerary.gif", 
+		"URL=http://localhost:1080/WebTours/images/itinerary.gif", 
+		"Method=GET", 
+		"Resource=1", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t17.inf", 
+		LAST);
+
+	web_custom_request("signoff.gif", 
+		"URL=http://localhost:1080/WebTours/images/signoff.gif", 
+		"Method=GET", 
+		"Resource=1", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t18.inf", 
+		LAST);
+
+	web_concurrent_end(NULL);
+
+	lr_end_transaction("02_Login",LR_AUTO);
+
+	lr_start_transaction("03_OpenBills");
+
+	web_revert_auto_header("Origin");
+
+	lr_think_time(13);
+
+	web_custom_request("Itinerary Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=itinerary", 
+		"Method=GET", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t19.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_concurrent_start(NULL);
+
+	web_custom_request("nav.pl_4", 
+		"URL=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=itinerary", 
+		"Method=GET", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?page=itinerary", 
+		"Snapshot=t20.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_custom_request("itinerary.pl", 
+		"URL=http://localhost:1080/cgi-bin/itinerary.pl", 
+		"Method=GET", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?page=itinerary", 
+		"Snapshot=t21.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_concurrent_end(NULL);
+
+	web_concurrent_start(NULL);
+
+	web_custom_request("cancelreservation.gif", 
+		"URL=http://localhost:1080/WebTours/images/cancelreservation.gif", 
+		"Method=GET", 
+		"Resource=1", 
+		"RecContentType=image/gif", 
+		"Referer=http://localhost:1080/cgi-bin/itinerary.pl", 
+		"Snapshot=t22.inf", 
+		LAST);
+
+	web_custom_request("cancelallreservations.gif", 
+		"URL=http://localhost:1080/WebTours/images/cancelallreservations.gif", 
+		"Method=GET", 
+		"Resource=1", 
+		"RecContentType=image/gif", 
+		"Referer=http://localhost:1080/cgi-bin/itinerary.pl", 
+		"Snapshot=t23.inf", 
+		LAST);
+
+	web_concurrent_end(NULL);
+
+	web_concurrent_start(NULL);
+
+	web_custom_request("in_itinerary.gif", 
+		"URL=http://localhost:1080/WebTours/images/in_itinerary.gif", 
+		"Method=GET", 
+		"Resource=1", 
+		"RecContentType=image/gif", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=itinerary", 
+		"Snapshot=t24.inf", 
+		LAST);
+
+	web_custom_request("home.gif", 
+		"URL=http://localhost:1080/WebTours/images/home.gif", 
+		"Method=GET", 
+		"Resource=1", 
+		"RecContentType=image/gif", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=itinerary", 
+		"Snapshot=t25.inf", 
+		LAST);
+
+	web_concurrent_end(NULL);
+
+	lr_end_transaction("03_OpenBills",LR_AUTO);
+
+	lr_start_transaction("04_DelBill");
 
 	web_add_header("Origin", 
 		"http://localhost:1080");
 
-	web_submit_form("itinerary.pl", 
-		"Snapshot=t6.inf", 
-		ITEMDATA, 
-		"Name=1", "Value=on", ENDITEM, 
+	lr_think_time(38);
+
+	web_custom_request("itinerary.pl_2", 
+		"URL=http://localhost:1080/cgi-bin/itinerary.pl", 
+		"Method=POST", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/itinerary.pl", 
+		"Snapshot=t26.inf", 
+		"Mode=HTTP", 
+		"Body=1=on&flightID=0-7-JD&removeFlights.x=49&removeFlights.y=7&.cgifields=1", 
 		LAST);
+
+	lr_end_transaction("04_DelBill",LR_AUTO);
+
+	lr_think_time(15);
+
+	lr_start_transaction("05_Logout");
+
+	web_custom_request("SignOff Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"Method=GET", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=itinerary", 
+		"Snapshot=t27.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	web_custom_request("nav.pl_5", 
+		"URL=http://localhost:1080/cgi-bin/nav.pl?in=home", 
+		"Method=GET", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"Snapshot=t28.inf", 
+		"Mode=HTTP", 
+		LAST);
+
+	lr_end_transaction("05_Logout",LR_AUTO);
+
+	lr_end_transaction("UC5_DelSel",LR_AUTO);
 
 	return 0;
 }
--- DelBill/pre_cci.c
+++ DelBill/pre_cci.c
@@ -2596,57 +2596,49 @@ vuser_init()
 # 1 "Action.c" 1
 Action()
 {
-	lr_start_transaction("UC5_DelSel");
+    int i;
+    lr_start_transaction("UC5_DelSel");
 
-	lr_start_transaction("01_OpenWebTours");
+    lr_start_transaction("01_OpenWebTours");
 
-	
-	web_set_sockets_option("SSL_VERSION", "2&3");
+    web_set_sockets_option("SSL_VERSION", "2&3");
 
-	web_add_auto_header("Priority", 
-		"u=0, i");
+    web_add_auto_header("Priority", "u=0, i");
+    web_add_auto_header("Upgrade-Insecure-Requests", "1");
 
-	web_add_auto_header("Upgrade-Insecure-Requests", 
-		"1");
-	web_reg_find("Text=Welcome to the Web Tours site.","LAST");
+    web_reg_find("Text=Welcome to the Web Tours site.", "LAST");
 
-	web_url("WebTours", 
-		"URL=http://localhost:1080/WebTours/", 
-		"Resource=0", 
-		"RecContentType=text/html", 
-		"Referer=", 
-		"Snapshot=t2.inf", 
-		"Mode=HTML", 
+    web_url("WebTours",
+        "URL=http://localhost:1080/WebTours/",
+        "Resource=0",
+        "RecContentType=text/html",
+        "Referer=",
+        "Snapshot=t2.inf",
+        "Mode=HTML",
+        "LAST");
+
+    lr_end_transaction("01_OpenWebTours", 2);
+
+    lr_start_transaction("02_Login");
+
+    web_reg_find("Text=Welcome, <b>{login}</b>, to the Web Tours reservation pages.", "LAST");
+    web_add_header("Origin", "http://localhost:1080");
+    web_add_auto_header("Priority", "u=4");
+
+    web_submit_form("login.pl",
+        "Snapshot=t3.inf",
+        "ITEMDATA",
+        "Name=username", "Value={login}", "ENDITEM",
+        "Name=password", "Value={password}", "ENDITEM",
+        "LAST");
+
+    lr_end_transaction("02_Login", 2);
+    
+    	lr_start_transaction("03_OpenBills");
+    web_reg_save_param_ex("ParamName=c_flightids", "LB=flightID\" value=\"", "RB=\"", "Ordinal=ALL", "LAST");
+    web_reg_save_param_ex("ParamName=c_cgifields", "LB=cgifields\" value=\"", "RB=\"", "Ordinal=ALL", "LAST");
+	web_reg_find("Text=<b>Itinerary</font></b>",
 		"LAST");
-
-
-	lr_end_transaction("01_OpenWebTours",2);
-
-	lr_start_transaction("02_Login");
-	web_reg_find("Text=Welcome, <b>{login}</b>, to the Web Tours reservation pages.","LAST");
-	web_add_header("Origin", 
-		"http://localhost:1080");
-
-	web_add_auto_header("Priority", 
-		"u=4");
-
-	
-	web_submit_form("login.pl", 
-		"Snapshot=t3.inf", 
-		"ITEMDATA", 
-		"Name=username", "Value={login}", "ENDITEM", 
-		"Name=password", "Value={password}", "ENDITEM", 
-		"LAST");
-
-	lr_end_transaction("02_Login",2);
-		web_reg_save_param_ex(
-		"ParamName=flightID",
-		"LB=flightID\" value\=\"",
-		"RB=\"",
-		"Ordinal=ALL",
-		"LAST");
-		lr_start_transaction("03_OpenBills");
-
 
 	(web_remove_auto_header("Upgrade-Insecure-Requests", "ImplicitGen=Yes", "LAST"));
 
@@ -2660,37 +2652,77 @@ Action()
 
 	lr_end_transaction("03_OpenBills",2);
 
-	lr_start_transaction("04_DelBill");
- 	lr_save_string(lr_paramarr_random("flightID"),"RandflightID");
- 			web_reg_find("Text={RandflightID}","Fail=Found",
-		"LAST");
-	web_add_header("Origin", 
-		"http://localhost:1080");
-
-	web_submit_form("itinerary.pl", 
-		"Snapshot=t6.inf", 
-		"ITEMDATA", 
-		"Name=1", "Value=on", "ENDITEM", 
-		"Name=flightID", "Value={RandflightID}", "ENDITEM", 
-		"Name=removeFlights.x", "Value=78", "ENDITEM", 
-		"Name=removeFlights.y", "Value=1", "ENDITEM", 
-		"LAST");
-	lr_end_transaction("04_DelBill", 2);
 
 
-	web_reg_find("Text=Welcome to the Web Tours site.","LAST");
+    lr_start_transaction("04_DelBill");
+
+    lr_save_string("", "c_buffer");
+
+    for (i=1;i<=atoi(lr_eval_string("{c_flightids_count}"));i++)
+    {
+    	lr_save_string(lr_paramarr_random("c_cgifields"),"c_rand");
+
+		lr_param_sprintf("c_buffer", "%s%d=on&", lr_eval_string("{c_buffer}"), atoi(lr_eval_string("{c_rand}")));
+
+    }
+
+
+    
+
+
+
+
+    for (i=1;i<=atoi(lr_eval_string("{c_flightids_count}"));i++)
+    {
+        lr_param_sprintf("c_buffer",
+        "%sflightID=%s&",
+        lr_eval_string("{c_buffer}"),
+        lr_paramarr_idx("c_flightids",
+        i));
+
+        lr_param_sprintf("c_buffer",
+        "%s.cgifields=%s&",
+        lr_eval_string("{c_buffer}"),
+        lr_paramarr_idx("c_cgifields",
+        i));
+    }
+
+
+    lr_save_string(lr_eval_string("{c_buffer}removeFlights.x=57&removeFlights.y=10&"), "c_wcr");
+
+    lr_save_string(lr_eval_string(lr_eval_string("{c_flightids_{c_flightids_count}}")),
+    "c_cancelflight");
+
+
+    web_reg_find("Text={c_cancelflight}", "Fail=Found", "LAST");
+
+
+    web_custom_request("itinerary.pl_2",
+    "URL=http://localhost:1080/cgi-bin/itinerary.pl",
+    "Method=POST",
+    "Resource=0",
+    "RecContentType=text/html",
+    "Referer=http://localhost:1080/cgi-bin/itinerary.pl",
+    "Snapshot=t23.inf",
+    "Mode=HTTP",
+    "Body={c_wcr}",
+    "LAST");
+
+    lr_end_transaction("04_DelBill", 2);
 	lr_start_transaction("05_Logout");
-
-	web_image("SignOff Button", 
-		"Alt=SignOff Button", 
-		"Snapshot=t4.inf", 
-		"LAST");
-
+	web_reg_find("Text=Welcome to the Web Tours site.","LAST");
+	web_url("welcome.pl_2", 
+    "URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+    "Resource=0", 
+    "RecContentType=text/html", 
+    "Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=itinerary", 
+    "Snapshot=t5.inf", 
+    "Mode=HTML", 
+    "LAST");
 	lr_end_transaction("05_Logout",2);
-	lr_end_transaction("UC5_DelSel", 2);
+    lr_end_transaction("UC5_DelSel", 2);
 
-
-	return 0;
+    return 0;
 }
 # 5 "c:\\users\\anton\\documents\\vugen\\scripts\\delbill\\\\combined_DelBill.c" 2
 
